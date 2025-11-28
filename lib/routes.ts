e38cff44bf972965d0e22c2635aeb4a27e84e498
Humbat Jamalov <143912059+Camalzadeh@users.noreply.github.com>
2025-11-28 16:05:17 +0400

    all fixed

--- lib/routes.ts
+++ lib/routes.ts
@@ -7,5 +7,6 @@ export const UI_PATHS = {
 export const API_ROUTES = {
     POLLS: '/api/polls',
     POLL_DETAIL: (id: string) => `/api/poll/${id}`,
+    VOTE_API: (id: string) => `/api/poll/${id}/vote`,
     POLL_RESULTS: (id: string) => `/api/poll/${id}/results`,
 };
